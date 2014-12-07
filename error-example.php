6f779c6fbee99e568dafca4a77f177b696371e81
Luke Kyohere <KayLuke@users.noreply.github.com>
2014-12-07 14:29:56 -0600

    Update error-example.php

--- error-example.php
+++ error-example.php
@@ -6,15 +6,4 @@ require( 'lib/Beyonic.php' );
 /* Set the API Key to be used in all requests */
 Beyonic::setApiKey( '6202349b8068b349b6e0b389be2a65cc36847c75' );
 
-/* Test Error */
-echo "**********\n";
-echo "Generating Error by getting Webhook of id 10000\n";
-try {
-	$hook = Beyonic_Webhook::get( 10000 );
-} catch (Beyonic_Exception $e) {
-	echo "{$e}\n";
-	echo "{$e->responseBody}\n";
-}
-echo "**********\n\n";
-
 ?>
