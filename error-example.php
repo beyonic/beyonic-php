19e261960ac88d7b0ccb180c7bfc562c668a399a
Luke Kyohere <KayLuke@users.noreply.github.com>
2014-12-07 14:30:21 -0600

    Delete error-example.php

--- error-example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Require the lib/Beyonic.php to included to access the interface objects */
-require( 'lib/Beyonic.php' );
-
-/* Set the API Key to be used in all requests */
-Beyonic::setApiKey( '6202349b8068b349b6e0b389be2a65cc36847c75' );
-
-?>
