677636a653235ab1bd734de7586337b9f9aec183
Nick Hargreaves <hargreaves1234@gmail.com>
2018-02-07 19:55:45 +0300

    update api path

--- lib/Beyonic.php
+++ lib/Beyonic.php
@@ -1,5 +1,5 @@
 <?php
-define("BEYONIC_CLIENT_VERSION", "0.0.6");
+define("BEYONIC_CLIENT_VERSION", "0.0.7");
 
 if (!function_exists('curl_init')) {
   throw new Exception('Beyonic requires the CURL PHP extension.');
